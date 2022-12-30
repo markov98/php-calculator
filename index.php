182aa1d23e5679a8823b8f21fd40cce4b4c9f17d
markov98 <74935630+markov98@users.noreply.github.com>
2022-12-30 11:38:47 +0200

    used a switch statement

--- index.php
+++ index.php
@@ -1,63 +1,75 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Calculator</title>
-        <meta charset="utf8">
-        <link rel="stylesheet" type="text/css" href="style.css">
-        <link rel="icon" type="image/png" href="pictures/numbers.png">    
-    </head>
-    <body>
 
-        <div class="image">
-            <img src="pictures/numbers.png">
-        </div>
+<head>
+    <title>Calculator</title>
+    <meta charset="utf8">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="icon" type="image/png" href="pictures/numbers.png">
+</head>
 
-        <header>
-            Calculator
-        </header>
+<body>
 
-        <form action="index.php" method="POST">
-            <input type="number" name='num1'/><br/>
-            <select name="operator">
-                <option>+</option>
-                <option>-</option>
-                <option>*</option>
-                <option>/</option>
-            </select><br/>
-            <input type="number" name='num2'/><br/>
-            <button type='submit' name="submit" value='submit'>=</button>
-        </form>
-        
-        <footer class="result">
-            <?php
-                if (isset($_POST['submit'])) {
-                    $num1 = sanitize_input($_POST['num1']);
-                    $num2 = sanitize_input($_POST['num2']);
-                    $operator = sanitize_input($_POST['operator']);
-                    if ($num1 == null || $num2 == null) {
-                        echo "No number provided";
-                    } elseif ($operator == '+') {
+    <div class="image">
+        <img src="pictures/numbers.png">
+    </div>
+
+    <header>
+        Calculator
+    </header>
+
+    <form action="index.php" method="POST">
+        <input type="text" name='num1' /><br />
+        <select name="operator">
+            <option>+</option>
+            <option>-</option>
+            <option>*</option>
+            <option>/</option>
+        </select><br />
+        <input type="text" name='num2' /><br />
+        <button type='submit' name="submit" value='submit'>=</button>
+    </form>
+
+    <footer class="result">
+        <?php
+        if (isset($_POST['submit'])) {
+            $num1 = sanitize_input($_POST['num1']);
+            $num2 = sanitize_input($_POST['num2']);
+            $operator = sanitize_input($_POST['operator']);
+
+            if (is_numeric($num1) && is_numeric($num2)) {
+                switch ($operator) {
+                    case "+":
                         echo $num1 + $num2;
-                    } elseif ($operator == '-') {
+                        break;
+                    case "-":
                         echo $num1 - $num2;
-                    } elseif ($operator == '*') {
+                        break;
+                    case "*":
                         echo $num1 * $num2;
-                    } elseif ($operator == '/') {
-                        if ($num2 == 0) {
-                            echo "Cannot divide by zero";
-                        } else {
+                        break;
+                    case "/":
+                        if ($num2 != 0) {
                             echo $num1 / $num2;
+                        } else {
+                            echo "Cannot divide by zero!";
                         }
-                    }
+                        break;
                 }
+            } else {
+                echo "Invalid input";
+            }
+        }
 
-                function sanitize_input($data) {
-                    $data = trim($data);
-                    $data = stripslashes($data);
-                    $data = htmlspecialchars($data);
-                    return $data;
-                }
-            ?>
-        </footer>
-    </body>
+        function sanitize_input($data)
+        {
+            $data = trim($data);
+            $data = stripslashes($data);
+            $data = htmlspecialchars($data);
+            return $data;
+        }
+        ?>
+    </footer>
+</body>
+
 </html>
